43216ed819df131ff99a0855bfbb3cc1cf1cde92
Yanislav Pavlov <mail@yweb.dev>
2024-05-05 12:28:40 +0200

    create summary section and period selection

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <script setup>
+import { transactionViewOptions } from "~/composable/constants.js";
 
+const selectedView = ref(transactionViewOptions[1])
 </script>
 
 <template>
-  <div>Index page</div>
+  <section>
+    <h1>
+      Summary
+    </h1>
+    <div>
+      <USelectMenu v-model="selectedView" :options="transactionViewOptions" />
+    </div>
+  </section>
 </template>
 
 <style scoped>
+  section {
+    @apply
+    flex items-center justify-between
+    mb-10
+  }
 
+  h1 {
+    @apply
+    text-4xl font-extrabold
+  }
 </style>
