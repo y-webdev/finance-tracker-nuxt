457f1fc11ca52afdded03e724887ed7dc9067b65
Yanislav Pavlov <mail@yweb.dev>
2024-05-15 13:23:15 +0200

    create custom modal component adding transactions

---
+++ components/transaction-modal.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  modelValue: Boolean,
+})
+const emit = defineEmits('update:modelValue')
+
+const isOpen = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+})
+</script>
+
+<template>
+  <UModal v-model="isOpen">
+    <UCard>
+      <template #header>
+        Add Transaction
+      </template>
+
+      <div>Hello!</div>
+    </UCard>
+  </UModal>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -78,15 +78,7 @@ console.log(transactionsGroupedByDate.value)
       </div>
     </div>
     <div>
-      <UModal v-model="isOpen">
-        <UCard>
-          <template #header>
-            Add Transaction
-          </template>
-
-          <div>Hello!</div>
-        </UCard>
-      </UModal>
+      <TransactionModal v-model="isOpen" />
       <UButton
           icon="i-heroicons-plus-circle"
           color="white"
