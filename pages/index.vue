35247d5beb1ae5466c3a39711d7f34dfbcd421dd
Yanislav Pavlov <mail@yweb.dev>
2024-05-15 13:01:12 +0200

    transactions total

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,20 @@ const selectedView = ref(transactionViewOptions[1])
 const supabase = useSupabaseClient()
 const transactions = ref([])
 const isLoading = ref(false)
+const income = computed(
+    () => transactions.value.filter(t => t.type === 'Income')
+)
+const expense = computed(
+    () => transactions.value.filter(t => t.type === 'Expense')
+)
+const incomeCount = computed(() => income.value.length)
+const expenseCount = computed(() => expense.value.length)
+const incomeTotal = computed(
+    () => income.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
+const expenseTotal = computed(
+    () => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
 const fetchTransactions = async () => {
   isLoading.value = true
 
@@ -50,10 +64,21 @@ console.log(transactionsGroupedByDate.value)
     </div>
   </section>
   <section class="trend">
-    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="isLoading" />
-    <Trend color="red" title="Expense" :amount="4000" :last-amount="3000" :loading="isLoading" />
+    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="isLoading" />
+    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="3000" :loading="isLoading" />
     <Trend color="green" title="Investments" :amount="4000" :last-amount="5000" :loading="isLoading" />
     <Trend color="red" title="Saving" :amount="4000" :last-amount="4100" :loading="isLoading" />
+  </section>
+  <section class="flex justify-between mb-10">
+    <div>
+      <h2 class="text-2xl font-extrabold">Transactions</h2>
+      <div class="text-gray-500 dark:text-gray-400">
+        You have {{ incomeCount }} incomes and {{ expenseCount }} expenses this period
+      </div>
+    </div>
+    <div>
+      <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" />
+    </div>
   </section>
   <section v-if="!isLoading">
     <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
