facd9b981bc488e8215c0343477d03ee85688a34
Yanislav Pavlov <mail@yweb.dev>
2024-05-15 13:52:35 +0200

    add inputs add constants

--- constants.js
+++ constants.js
@@ -1,1 +1,3 @@
 export const transactionViewOptions = ['Yearly', 'Monthly', 'Weekly', 'Daily']
+export const categories = ['Food', 'Housing', 'Car', 'Entertainment']
+export const types = ['Income', 'Expense', 'Saving', 'Investment']
