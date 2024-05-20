3016abb169ab93044162aa45ea82b743c20ee723
Yanislav Pavlov <mail@yweb.dev>
2024-05-20 13:01:21 +0200

    signout implementation

--- components/app-header.vue
+++ components/app-header.vue
@@ -1,13 +1,65 @@
+<script setup>
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const items = [
+  [
+      {
+        label: user.value?.email,
+        slot: 'account',
+        disabled: true
+      }
+  ],
+  [
+      {
+        label: 'Settings',
+        icon: 'i-heroicons-cog-8-tooth',
+        onClick: () => console.log('Link to settings in the future')
+      },
+      {
+        label: 'Sign out',
+        icon: 'i-heroicons-arrow-left-on-rectangle',
+        click: async () => {
+          await supabase.auth.signOut()
+          return navigateTo('/login')
+        }
+      }
+  ]
+]
+</script>
 <template>
   <header>
     <NuxtLink to="/" class="link">
       Finance Tracker
     </NuxtLink>
     <div>
-      <UAvatar
-          src="https://avatars.githubusercontent.com/u/739984?v=4"
-          alt="Avatar"
-      />
+      <UDropdown
+          :items="items"
+          :ui="{
+            item: {
+              disabled: 'cursor-text select-text'
+              },
+              width: 'w-64'
+          }"
+          v-if="user">
+        <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+
+        <template #account="{ item }">
+          <div class="text-left">
+            <p>
+              Signed in as
+            </p>
+            <p class="font-medium text-gray-900 dark:text-white">
+              {{ item.label }}
+            </p>
+          </div>
+        </template>
+
+        <template #item="{ item }">
+          <span class="truncate">{{ item.label }}</span>
+
+          <UIcon :name="item.icon" class="icon" />
+        </template>
+      </UDropdown>
     </div>
   </header>
 </template>
@@ -22,5 +74,13 @@
   header .link {
     @apply
     text-xl font-bold
+  }
+
+  .icon {
+    @apply
+    flex-shrink-0
+    h-4 w-4
+    text-gray-400 dark:text-gray-500
+    ms-auto
   }
 </style>
